be479a888221f682282377add933218bce23c3f3
LionPK <praneed.kla@northbkk.ac.th>
2018-02-07 10:58:54 +0700

    Initial commit

---
+++ application/views/employee_form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Employee Records in Database</title>
+</head>
+<body>
+	<form action="<?php echo site_url('employee/employee_form');?>" method="post">
+		<table align="center">
+			<tr>
+				<td>Name:</td>
+				<td><?php echo form_input(array('id'=>'emp_name', 'name'=>'emp_name', 'placeholder'=>'Name', 'size'=>25)); ?></td>
+			</tr>
+			<tr>
+				<td>Gender:</td>
+				<td><?php echo form_input(array('id'=>'emp_gender','name'=>'emp_gender','placeholder'=>'Gender', 'size'=>25));?></td>
+			</tr>
+			<tr>
+				<td>Email:</td>
+				<td><?php echo form_input(array('id'=>'emp_email','name'=>'emp_email','placeholder'=>'Email', 'size'=>25));?></td>
+			</tr>
+			<tr>
+				<td>Phone:</td>
+				<td><?php echo form_input(array('id'=>'emp_phone','name'=>'emp_phone','placeholder'=>'Phone', 'size'=>25));?></td>
+			</tr>
+			<tr>
+				<td>Address:</td>
+				<td><?php echo form_input(array('type'=>'textarea','id'=>'emp_address', 'name'=>'emp_address','placeholder'=>'Address', 'size'=>25));?></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><button type="submit" id="employee-submit">ADD</button>
+				</td>
+			</tr>
+		</table>
+	</form>
+
+</body>
+</html>
